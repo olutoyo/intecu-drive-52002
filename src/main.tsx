dcf201b10fe5010fb34169a2c00ca67c258de415
olutoyo <105394070+olutoyo@users.noreply.github.com>
2025-10-24 17:42:38 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,8 @@
-// import { createRoot } from "react-dom/client";
-// import App from "./App.tsx";
-// import "./index.css";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-// createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(<App />);
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
 
