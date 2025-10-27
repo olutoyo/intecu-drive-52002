381809909358cc8384d92f58b7995b913af6b776
olutoyo <105394070+olutoyo@users.noreply.github.com>
2025-10-27 11:03:35 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base: '/intecu-drive-52002/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
